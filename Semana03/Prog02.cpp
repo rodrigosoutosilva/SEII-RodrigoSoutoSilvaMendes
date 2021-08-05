e392850451bc2dbd725b65a3db6cefad793e9fe8
rodrigosoutosilva <80899931+rodrigosoutosilva@users.noreply.github.com>
2021-08-04 23:38:00 -0300

    Add files via upload

---
+++ Semana03/Prog02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void celebrate(int age);
+
+
+int main()
+{
+	int myage = 26;
+	cout << "BF " << myage << endl;
+	celebrate(myage);
+
+
+	system("pause>0");
+}
+
+void celebrate(int age)
+{
+	age++
+		count << "HEEEY   " << age << "  ANOS" << endl;
+}
