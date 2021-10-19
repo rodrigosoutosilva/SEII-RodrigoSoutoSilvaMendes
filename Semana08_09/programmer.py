f1f5453bd6e518f8ffbcf6a5b7e47028d803a761
rodrigosoutosilva <80899931+rodrigosoutosilva@users.noreply.github.com>
2021-10-19 11:08:30 -0300

    Add files via upload

---
+++ Semana08_09/programmer.py
@@ -0,0 +1,23 @@
+
+import sys
+from PyQt5.QtWidgets import QApplication, QLabel, QPushButton, QVBoxLayout, QWidget, QFileDialog, QGridLayout
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtGui, QtCore
+from PyQt5.QtGui import QCursor
+
+from functions import frame1, frame2, frame3, frame4, grid
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.setWindowTitle("Who wants to be a programmer???")
+window.setFixedWidth(1000)
+
+window.setStyleSheet("background: #161219;")
+
+frame1()
+
+window.setLayout(grid)
+
+window.show()
+sys.exit(app.exec())
